e2a312aa5937d1b30382a94d544dae673cf2ab6b
iftikhar chetoui <115534833+foufa102022@users.noreply.github.com>
2024-02-13 10:59:45 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Ce script Python calcule la somme de deux nombres et affiche le résultat
+
+def calculer_somme(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    nombre1 = 5
+    nombre2 = 10
+    somme = calculer_somme(nombre1, nombre2)
+    print(f"La somme de {nombre1} et {nombre2} est égale à {somme}.")
